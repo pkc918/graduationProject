fb0c429dbf7c740927755e89409d9e9be3f94ba0
XiaoPan <2489964425@qq.com>
2021-04-20 15:45:46 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+import Home from './views/Home/home';
+import Login from './views/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">login</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/views/Login/Login.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>login</div>
+  );
+}
+
+export default Home;
