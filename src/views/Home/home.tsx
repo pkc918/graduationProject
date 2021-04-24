be670bc8e10d5af1d9c7424652cc91d4b6477c58
XiaoPan <2489964425@qq.com>
2021-04-24 15:52:46 +0800

    饼图，柱状图完成

--- src/views/Home/home.tsx
+++ src/views/Home/home.tsx
@@ -1,15 +1,43 @@
 import React, {FC, useEffect, useState} from 'react';
 import EChartsReact from 'echarts-for-react';
 import request from '../../request/request';
+import styled from 'styled-components';
 
 const Home: FC = () => {
+  const Main = styled.main`
+    height: 100vh;
+    display: grid;
+    justify-content: center;
+    align-items: center;
+    //background: url("https://xiaopan-struggle.github.io/Echats-Demo/images/bg.jpg") no-repeat;
+  `
+
   const [positionName, setPositionName] = useState(['']);
   const [positionNumber, setPositionNumber] = useState([5000]);
+  const [education,setEducation] = useState([{}]);
+  const getEducation = () => {
+    let educations: Array<object> = [];
+    request('/spider-position/education-num','POST')
+      .then(res => {
+        console.log(res);
+        educations = res.data.data.map((item: {education: string, requiredNum: number}) => {
+          return {name: item.education,value: item.requiredNum};
+        });
+        setEducation(educations);
+      })
+  }
+  const getSalary = () => {
+    request('/spider-position/salary-num','POST',{searchName: '大数据'})
+      .then(res => {
+        console.log(res);
+      })
+  }
   useEffect(() => {
     let profession: Array<string> = [];
     let demand: Array<number> = [];
     request('/spider-position/position-num', 'POST')
       .then(res => {
+        console.log(res.data);
         let data = res.data.data;
         for (let item of data) {
           profession.push(item.positionName);
@@ -18,8 +46,10 @@ const Home: FC = () => {
         setPositionName(profession);
         setPositionNumber(demand)
       });
+    getEducation();
+    getSalary();
   },[]);
-  let option = {
+  let lineOption = {
     title: {
       text: '职位需求量',
       textAlign: 'auto'
@@ -32,9 +62,13 @@ const Home: FC = () => {
         }
       }
     },
+    axisTick: {
+      alignWithLabel: true
+    },
+    // interval: 30,
     grid: {
-      left: '3%',
-      right: '4%',
+      left: '0',
+      right: '13%',
       bottom: '3%',
       containLabel: true
     },
@@ -59,13 +93,108 @@ const Home: FC = () => {
       },
     }]
   };
+  let pieOption = {
+    title: {
+      text: '学历要求统计',
+      left: 'center'
+    },
+    tooltip: {
+      trigger: 'item'
+    },
+    series: [
+      {
+        name: '访问来源',
+        type: 'pie',
+        radius: '50%',
+        data: education,
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
+  };
+  let barOption =  {
+    title: {
+      text: '职位薪资',
+      // textStyle: {
+      //   color: '#fff'
+      // }
+    },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow'
+      }
+    },
+    legend: {
+      data: ['5k以下', '5k~10k','10k~18k','20k~30k','30k以上'],
+      // textStyle: {
+      //   color: '#fff'
+      // }
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
+    xAxis: {
+      type: 'value',
+      boundaryGap: [0, 0.01]
+    },
+    yAxis: {
+      type: 'category',
+      data: ['前端','Java','大数据','Python','全栈']
+    },
+    series: [
+      {
+        name: '5k以下',
+        type: 'bar',
+        data: [18203, 23489, 104970, 131744, 630230]
+      },
+      {
+        name: '5k~10k',
+        type: 'bar',
+        data: [19325, 23438, 121594, 134141, 681807]
+      },
+      {
+        name: '10k~18k',
+        type: 'bar',
+        data: [19325, 23438, 121594, 134141, 681807]
+      },
+      {
+        name: '20k~30k',
+        type: 'bar',
+        data: [19325, 23438, 121594, 134141, 681807]
+      },
+      {
+        name: '30k以上',
+        type: 'bar',
+        data: [19325, 31000, 121594, 134141, 681807]
+      }
+    ]
+  };
+
   return (
-    <div className="App">
+    <Main>
       <EChartsReact
-        option={option}
-        style={{width: '50%'}}
+        option={barOption}
+        style={{width: '800px',height:'600px'}}
       />
-    </div>
+      <EChartsReact
+        option={lineOption}
+        style={{width: '500px',height: '300px'}}
+      />
+      <EChartsReact
+        option={pieOption}
+        style={{height: '600px'}}
+      />
+
+    </Main>
   );
 };
 
