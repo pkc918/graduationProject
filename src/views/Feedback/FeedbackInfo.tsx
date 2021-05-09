ca93980c9c1ab967489a056e1a3eea7625669e22
XiaoPan <2489964425@qq.com>
2021-05-09 20:20:37 +0800

    用户反馈

--- src/views/Feedback/FeedbackInfo.tsx
+++ src/views/Feedback/FeedbackInfo.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
-import {FC} from 'react';
+import {FC, useEffect, useState} from 'react';
 import {NavBar} from '../../components/Nav';
+import request from '../../request/request';
+import {Pagination} from 'antd';
 
 const Main = styled.main`
   width: 100%;
@@ -8,22 +10,97 @@ const Main = styled.main`
   display: flex;
   overflow: hidden;
 
-  > .main{
+  > .main {
     width: 100%;
     padding: 30px;
     display: grid;
+    overflow-y: auto;
+
+    section {
+      width: 900px;
+      border: 1px #000 dashed;
+      padding: 10px;
+      margin-bottom: 40px;
+
+      h3 {
+        width: 100%;
+        height: 40px;
+        font-size: 20px;
+        text-align: center;
+        line-height: 40px;
+        background-color: #ffccc7;
+        border-radius: 20px;
+        margin-bottom: 10px;
+      }
+
+      main {
+        width: 100%;
+        height: 100px;
+        word-wrap: break-word;
+        overflow-y: auto;
+        background-color: #f9e1bb;
+        font-size: 18px;
+        font-family: "SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace;
+
+        &::-webkit-scrollbar {
+          display: none;
+        }
+      }
+    }
   }
-`
+
+  .ant-pagination-options {
+    display: none;
+  }
+`;
 
 const FeedbackInfo: FC = () => {
-  return(
+  /*反馈数据*/
+  const [tableData, setTableData] = useState([]);
+  const [total, setTotal] = useState(1);
+  const [page,setPage] = useState(1);
+  const [pageSize] = useState(4);
+  /*分页*/
+  const handleChange = (page: number) => {
+    setPage(() => {
+      return page
+    })
+  }
+
+  useEffect(() => {
+    request('/feedback/selectPage', 'GET', {'pageNum':page,pageSize})
+      .then(res => {
+        console.log('反馈');
+        console.log(res.data.data);
+        setTotal(res.data.data.total);
+        setTableData(res.data.data.records);
+      });
+  }, [page]);
+
+  return (
     <Main>
-      <NavBar id={3} />
+      <NavBar id={3}/>
       <div className="main">
-        123
+        {
+          tableData.map(item => {
+            return (
+              <section key={Math.random()}>
+                {/* @ts-ignore*/}
+                <h3>{item.createTime}</h3>
+                {/* @ts-ignore*/}
+                <main>{item.feedBack}</main>
+              </section>
+            );
+          })
+        }
+        <Pagination
+          defaultCurrent={1}
+          current={page}
+          onChange={(page) => {handleChange(page)}}
+          total={total}/>
       </div>
     </Main>
-  )
-}
+  );
+};
 
-export default FeedbackInfo
+export default FeedbackInfo;
