18cf5048c650c680e4f4278579b22c34c4dce64f
XiaoPan <2489964425@qq.com>
2021-05-17 17:50:59 +0800

    用户发布反馈

---
+++ src/views/MyFeedback/MyFeedback.tsx
@@ -0,0 +1,61 @@
+import React, {FC, useState} from 'react';
+import {Button, Input} from 'antd';
+import {NavBar} from '../../components/Nav';
+import styled from 'styled-components';
+
+const Main = styled.main`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  overflow: hidden;
+
+  > .main {
+    width: 100%;
+    padding: 30px;
+    box-sizing: border-box;
+    overflow-y: auto;
+    display: grid;
+    grid-template-rows: 500px 1fr;
+
+    .ant-input{
+      height: 500px;
+    }
+    button{
+      margin-top: 50px;
+    }
+  }
+`
+
+const MyFeedback:FC = () => {
+  const {TextArea} = Input;
+  const [content,setContent] = useState('');
+
+  const onChange = (e: { target: { value: any; }; }) => {
+    setContent(e.target.value);
+    console.log(e.target.value);
+  };
+  const submitContent = () => {
+    console.log(content);
+  }
+
+  return(
+    <Main>
+      <NavBar id={7}/>
+      <div className="main">
+        <TextArea
+          showCount
+          maxLength={300}
+          value={content}
+          onChange={onChange} />
+        <Button
+          type="primary"
+          onClick={submitContent}
+        >
+          提交
+        </Button>
+      </div>
+    </Main>
+  )
+}
+
+export {MyFeedback}
